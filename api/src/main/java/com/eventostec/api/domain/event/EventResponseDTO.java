3a4466773b652550f64d5d1b83135e20bc6b67c2
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2024-10-24 15:05:41 -0300

    feature: criação de serviço para adicionar cupom a evento

---
+++ api/src/main/java/com/eventostec/api/domain/event/EventResponseDTO.java
@@ -0,0 +1,18 @@
+package com.eventostec.api.domain.event;
+
+import java.util.Date;
+import java.util.UUID;
+
+public record EventResponseDTO(
+	UUID id, 
+	String title, 
+	String description,
+	Date date,
+	String city,
+	String state,
+	Boolean remote,
+	String imgUrl,
+	String eventUrl
+){
+
+}
--- api/src/main/java/com/eventostec/api/services/EventService.java
+++ api/src/main/java/com/eventostec/api/services/EventService.java
@@ -4,16 +4,21 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.repositories.EventRepository;
 
 @Service
@@ -50,6 +55,24 @@ public class EventService {
 		return newEvent;
 	}
 	
+	/*
+	public List<EventResponseDTO> getEvents(int page, int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		Page<Event> eventsPage = this.eventRepository.findAll(pageable);
+		return eventsPage.map(
+			event -> new EventResponseDTO(event.getId(), 
+				event.getTitle(), 
+				event.getDescription(),
+				event.getDate(),
+				"",
+				"",
+				event.getRemote(),
+				event.getEventUrl(),
+				event.getImgUrl()
+			)
+		).stream().toList();
+	}
+	*/
 	
 	private String uploadImage(MultipartFile multipartFile) {
 		
